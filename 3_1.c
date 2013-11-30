ad9d3773bd4035ce7d6a5cfd3b62c8200f29f68e
Junggu Lee <hyde1004@gmail.com>
2013-11-30 23:00:27 +0900

    improve c file

--- 3_1.c
+++ 3_1.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include "div.h"
 
+int div(int a, int b);
 int main(int argc, char const *argv[])
 {
 	/* code */
 	printf("hello world\n");
-
+	printf("%d div %d = %d\n", 2, 3, div(2,3));
+	
 	return 0;
+}
+
+int div(int a, int b)
+{
+	return a/b;
 }
