2a975ad450dc5955559c2cf517babfc4b79fbe25
Junggu Lee <hyde1004@gmail.com>
2013-12-01 06:53:13 +0900

    add function is implemented

--- 3_1.c
+++ 3_1.c
@@ -6,6 +6,7 @@ int main(int argc, char const *argv[])
 	/* code */
 	printf("hello world\n");
 	printf("%d div %d = %d\n", 2, 3, div(2,3));
+	printf("%d add %d = %d\n", 2, 3, add(2,3));
 
 	return 0;
 }
@@ -13,4 +14,9 @@ int main(int argc, char const *argv[])
 int div(int a, int b)
 {
 	return a/b;
+}
+
+int add(int a, int b)
+{
+	return a+b;
 }
--- 3_1.h
+++ 3_1.h
@@ -1,4 +1,5 @@
 #ifndef __3_1_H__
 #define __3_1_H__
 int div(int a, int b);
+int add(int a, int b);
 #endif
